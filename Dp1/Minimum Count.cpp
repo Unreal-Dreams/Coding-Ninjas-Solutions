4612fe116e99321e8e00632e5c2c5b71f79d0273
Vaibhav Kumar Singh <56435229+vaibhavvats1000@users.noreply.github.com>
2021-01-08 00:11:50 +0530

    test

--- Dp1/Minimum Count.cpp
+++ Dp1/Minimum Count.cpp
@@ -6,6 +6,7 @@ Explanation:
 That is, if N is 4, then we can represent it as : {1^2 + 1^2 + 1^2 + 1^2} and {2^2}.The output will be 1,
 as 1 is the minimum count of numbers required to represent N as sum of squares.
 
+//This is the optimal solution
 
 Sol:
 #include <cmath>
